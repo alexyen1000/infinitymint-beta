ae9d9bbb60cc95ff7b5658e74bed497a426fcb9e
Llydia Cross <llydiacross@gmail.com>
2022-12-17 17:38:29 +0000

    s

--- app/interfaces.ts
+++ app/interfaces.ts
@@ -5,23 +5,32 @@ import { FuncSingle } from "./helpers";
 import { Server, ServerOptions } from "ganache";
 import { EventEmitter } from "events";
 import { DeploymentScript } from "./deployments";
+import InfinityConsole from "./console";
 
-export interface InfinityMintApplicationConfig {
-	productionChains: string[];
+export interface InfinityMintGemScript extends InfinityMintDeploymentScript {
+	name: string;
+	init?: FuncSingle<InfinityMintGemParameters, Promise<void>>;
 }
 
-/**
- * @interface
- * InfinityMint Project settings
- * @param name {string} - Name of the rpoejct
- * @param Specify custom @link InfinityLinks
- * @param system {Array<String>} Specify whether the project is part of a grouped system with other projects.
- * @param modules {InfinityMintProjectModules} - Specify which InfintyMint modules will be used within
-   the creation of tokens, such as *RarityMinter*, *RoyaltyController*.
-   This is an immutable setting that cannot be changed or added with more.
-	 They are specific to the InfinityMint source code.
-	 
- */
+export interface InfinityMintGemParameters
+	extends InfinityMintDeploymentParameters {
+	gem: InfinityMintGemScript;
+}
+
+export interface InfinityMintGemConfig {
+	name?: string;
+	homepage?: string;
+	git?: string;
+	version: string;
+	solidityNamespace: string;
+	author?: Dictionary<any> | Dictionary<any>[];
+}
+
+export interface InfinityMintApplicationConfig {
+	productionChains: any[];
+	testChains: any[];
+}
+
 export interface InfinityMintProject {
 	name: string;
 	infinityLinks?: Array<InfinityMintProjectSettingsLink>;
@@ -64,9 +73,9 @@ export interface InfinityMintProjectPermissions extends Dictionary<any> {
 	all?: Array<string>;
 	erc721?: Array<string>;
 	minter?: Array<string>;
-	random?: Array<string>;
-	royalty?: Array<string>;
-	assets?: Array<string>;
+	randomController?: Array<string>;
+	royaltyController?: Array<string>;
+	assetController?: Array<string>;
 	gems?: Array<string>;
 }
 
@@ -92,14 +101,30 @@ export interface InfinityMintProjectSettingsLinkSettings {
 }
 
 export interface InfinityMintProjectSettingsValues extends Dictionary<any> {
-	maxSupply?: number;
-	maxTokensPerWallet?: number;
-	approvedOnly?: boolean;
+	disablePreviews?: boolean;
+	disableAssets?: boolean;
 }
 
 export interface InfinityMintProjectSettingsMinter {
+	pathId?: number;
+	maxSupply?: number;
+	mintBytes?: Dictionary<any>;
+	approvedOnly?: boolean;
+	onlyImplicitMint?: boolean;
+	maxTokensPerWallet?: number;
+	previewCount?: number;
+}
+
+export interface InfinityMintProjectSettingsAssets {
 	incremental?: boolean;
 	randomNames?: boolean;
+	preventSamePathTwice?: boolean;
+	rarity?: {
+		pickLeastCommon?: boolean;
+		pickMostCommon?: boolean;
+	};
+	disabledPaths?: Array<any>;
+	nameCount?: number;
 }
 
 export interface InfinityMintProjectSettingsRoyaltySplit
@@ -121,17 +146,25 @@ export interface InfinityMintProjectSettingsRoyalty extends Dictionary<any> {
 		| InfinityMintProjectSettingsRoyaltySplit[];
 }
 
+export interface InfinityMintProjectSettingsERC721 extends Dictionary<any> {
+	defaultTokenURI?: string;
+	allowTransfer?: boolean;
+}
+
 export interface InfinityMintProjectSettings {
+	erc721?: InfinityMintProjectSettingsERC721;
 	minter?: InfinityMintProjectSettingsMinter;
 	values?: InfinityMintProjectSettingsValues;
 	royalty?: InfinityMintProjectSettingsRoyalty;
+	assets?: InfinityMintProjectSettingsAssets;
+	gems?: Dictionary<any>;
 }
 
 export interface InfinityMintProjectModules {
-	assets: string;
+	assetController: string;
 	minter: string;
-	royalty: string;
-	random: string;
+	royaltyController: string;
+	randomController: string;
 }
 
 export interface InfinityMintProjectPathExport {
@@ -168,35 +201,7 @@ export interface InfinityMintProjectAsset extends InfinityMintProjectPath {
 }
 
 /**
- * 
- * @interface
- * The InfinityMint project configuration. Is read in the first phase of running `npm run start`.
- * @param project {String} - The name of the project.
- * @param networks {Object} - The web3 network(s) InfinityMint will connect to.
- * @param hardhat {HardhatUserConfig} - The configuration that Hardhat will use to determine the versioning
-   & settings Solidity runtime.
- * @param ipfs {any} - The url to connect to IPFS, or set to false to not use IPFS.
-	 @param ganache {ServerOptions} The settings inputted to ganache within the `server.run()` function of its startup.
-	 Uses all valid configuration options found within their docs. <https://www.npmjs.com/package/ganache>
-	 For example;
-	 ```js
-	 ganache: {
-    chain: {
-      chainId: 1337,
-    },
-    wallet: {
-			// `wallet.totalAccounts`
-      totalAccounts: 20,
-      defaultBalance: 69420,
-    },
-  },
-	```
-	 @param imports {Array<String>} - Other filesystem locations where IM will look for assets
-	 such as PNGs, Vectors etc.to create tokens with.
-	 @param settings {InfinityMintConfigSettings} - InfinityMint-specific config settings.
-	 Configures settings such as how networks behave, what wallets to use by default,
-	 & if to log a specific chain within `defaultPipe`, & specify whether a chain is *production* or a *testnet*.
-	 Also determines what will act as the *Gas Machine.*
+ * Interface for the InfinityMint configuration file
  */
 export interface InfinityMintConfig {
 	project?: string;
@@ -235,6 +240,7 @@ export interface InfinityMintScriptArguments {
 	name: string;
 	optional?: boolean;
 	validator?: Function;
+	value?: any;
 }
 
 export interface InfinityMintConsole {
@@ -244,12 +250,21 @@ export interface InfinityMintConsole {
 export interface InfinityMintScript {
 	name?: string;
 	description?: string;
-	execute: Function;
+	execute: FuncSingle<InfinityMintScriptParameters, Promise<void>>;
 	arguments?: InfinityMintScriptArguments[];
+}
+
+export interface InfinityMintScriptParameters extends Dictionary<any> {
+	args?: Dictionary<InfinityMintScriptArguments>;
+	eventEmitter?: EventEmitter;
+	project?: InfinityMintProject;
+	log: FuncSingle<string, void>;
+	debugLog: FuncSingle<string, void>;
 }
 
 export interface InfinityMintDeploymentParameters extends Dictionary<any> {
 	setup?: boolean;
+	console?: InfinityConsole;
 	eventEmitter?: EventEmitter;
 	deployments?: Dictionary<any>;
 	deploy?: DeploymentScript;
