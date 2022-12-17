fcdc482ffd77b9b8ccf1b63930520a33aa4e6351
Llydia Cross <llydiacross@gmail.com>
2022-12-17 20:27:15 +0000

    s

---
+++ dist/gems/@/redemptionTools/client.d.ts
@@ -0,0 +1,3 @@
+export default client;
+declare const client: {};
+//# sourceMappingURL=client.d.ts.map
---
+++ dist/gems/@/redemptionTools/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const gem = {
+    name: "ERC721 Redemption Tools",
+    deploy: async () => { },
+    init: async (params) => { },
+    setup: async () => { },
+};
+exports.default = gem;
+//# sourceMappingURL=main.js.map
