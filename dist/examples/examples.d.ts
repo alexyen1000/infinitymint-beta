7962ac784d3d558f64f11afe0e3ddcaa578075b9
koma <m@kae.si>
2022-12-23 23:23:07 +0000

    Docs: automatic doc builds

---
+++ dist/examples/examples.d.ts
@@ -0,0 +1,31 @@
+/**
+ * Deploying a web3 contract with InfinityMint is very simple. You can either use the InfinityConsole to automate the deploy process of your contract or you can use a deploy script. This example will demonstrate the creation of an InfinityMint deploy script, assuming you are on TypeScript.
+ *
+ *
+ */
+export declare const DeploymentExample: {};
+/**
+ * InfinityMint automatically integrates ganache into a seamless developer
+ */
+export declare const GanacheExample: {};
+/**
+ * Modify InfinityMint through the use of gems, our extension system.
+ */
+export declare const GemsExample: {};
+/**
+ * An InfinityMint project is the container for your Web3 idea, see {@link app/interfaces.InfinityMintProject}.
+ */
+export declare const ProjectExample: {};
+/**
+ * InfinityMint can be ran in many different configurations and installed directly as an npm package and used as the developmental boilerplate for your web3 experiments.
+ */
+export declare const SetupExample: {};
+/**
+ * Test your code with InfinityMint. InfinityMint will handle the testing of Web3 and react code which can be integrated easily into CDI solutions.
+ */
+export declare const TestsExample: {};
+/**
+ * Create rich and detailed user experiences inside of the terminal window with the help of the InfinityMint windows system, see {@link app/window.InfinityMintWindow}
+ */
+export declare const WindowsExample: {};
+//# sourceMappingURL=examples.d.ts.map
---
+++ dist/examples/examples.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WindowsExample = exports.TestsExample = exports.SetupExample = exports.ProjectExample = exports.GemsExample = exports.GanacheExample = exports.DeploymentExample = void 0;
+/**
+ * Deploying a web3 contract with InfinityMint is very simple. You can either use the InfinityConsole to automate the deploy process of your contract or you can use a deploy script. This example will demonstrate the creation of an InfinityMint deploy script, assuming you are on TypeScript.
+ *
+ *
+ */
+exports.DeploymentExample = {};
+/**
+ * InfinityMint automatically integrates ganache into a seamless developer
+ */
+exports.GanacheExample = {};
+/**
+ * Modify InfinityMint through the use of gems, our extension system.
+ */
+exports.GemsExample = {};
+/**
+ * An InfinityMint project is the container for your Web3 idea, see {@link app/interfaces.InfinityMintProject}.
+ */
+exports.ProjectExample = {};
+/**
+ * InfinityMint can be ran in many different configurations and installed directly as an npm package and used as the developmental boilerplate for your web3 experiments.
+ */
+exports.SetupExample = {};
+/**
+ * Test your code with InfinityMint. InfinityMint will handle the testing of Web3 and react code which can be integrated easily into CDI solutions.
+ */
+exports.TestsExample = {};
+/**
+ * Create rich and detailed user experiences inside of the terminal window with the help of the InfinityMint windows system, see {@link app/window.InfinityMintWindow}
+ */
+exports.WindowsExample = {};
+//# sourceMappingURL=examples.js.map
