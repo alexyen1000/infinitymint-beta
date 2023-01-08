0a67dfd21da8c11144945373cf42a4abe7bc221a
koma <m@kae.si>
2023-01-08 02:19:27 +0000

    Feature: Add withdraw script for project contracts

---
+++ tests/scripts/withdraw.ts
@@ -0,0 +1,15 @@
+import Withdraw from '../../scripts/withdraw';
+
+describe('InfinityMint withdraw script', () => {
+	it('Should return the current owner address', () => {
+		Withdraw().shouldReturn(true);
+	});
+
+	it('Returns current project name correctly', () => {
+
+	});
+
+	it('Fails when current minter balance is 0', () => {
+
+	});
+});
